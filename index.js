50aaeead6fb5e14128b8375d922e4799660734cd
Duncan Kelly <leucochroic@gmail.com>
2016-12-14 13:08:38 -0600

    - redirects should be fully functional

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ var proxy = require('express-http-proxy');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-app.use('/tracks', proxy('sc-redirect.herokuapp.com/callback.html'));
+// app.use(express.static(__dirname + '/public'));
+app.use('/tracks', proxy('sc-redirect.herokuapp.com', {
+  forwardPath: function (req) {
+    var x = "http://sc-redirect.herokuapp.com/callback.html";
+    console.log(x);
+    return x;
+  }
+}));
+console.log("proxy going out");
+// app.use('/tracks', proxy('google.com'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
